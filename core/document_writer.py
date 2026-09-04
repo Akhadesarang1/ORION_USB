4da7a1e09452d73a2744c1a877ace73857eac908
Sarang Akhade <Akhadesarang1@gmail.com>
2026-09-04 10:17:26 +0530

    feat(fix) : fixed web socketing

--- core/document_writer.py
+++ core/document_writer.py
@@ -1,9 +1,5 @@
-
 import re
 from docx import Document
-
-
-
 from docx.shared import Pt, Inches, RGBColor
 from docx.enum.text import WD_ALIGN_PARAGRAPH, WD_LINE_SPACING
 from docx.enum.style import WD_STYLE_TYPE
